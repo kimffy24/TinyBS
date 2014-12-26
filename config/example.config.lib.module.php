262cd060859b47a125aa20b26633f1ae3841355c
kimffy24 <kimffy24@gmail.com>
2014-12-26 10:36:56 +0800

    reset example config file

--- config/example.config.lib.module.php
+++ config/example.config.lib.module.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * rule list as config/config.module.php
- * 
+ * rule list as config/config.lib.module.php
+ * @see example.config.module.php
  */
 return array(
-	'DemoLib'
+	'DemoLib',
+	array('SelfLibrary', '/server/project/phpSelfLibrary/phpSelfLibrary')
 );
--- config/example.config.module.php
+++ config/example.config.module.php
@@ -12,7 +12,10 @@
  *  if you offer an array like following
  *      return array(
  *  	   array('ModuleName', '/path/to/module/root'),
- *  	   array('module_name' => 'ModuleName', 'module_path' => '/path/to/module/root'),
+ *  	   array(
+ *  			'module_name' => 'ModuleName', 
+ *  			'module_path' => '/path/to/module/root'
+ *  		),
  *      );
  *  it contains two module outside tinybs.
  *  Infomation provided with per array.
@@ -21,8 +24,6 @@
  *  The other case is provides module name with the key module_name and provides module path with the key module_path. 
  */
 return array(
-	//'demo',
-	//'album'
 	'HelloWorldTinyBS',
-    array('Order', '/server/project/Order/Order')
+    //array('Order', '/server/project/Order/Order')
 );
