d16b697275b98342fc0553394c3e03726e03de46
kimffy24 <kimffy24@gmail.com>
2015-05-21 18:04:07 +0000

    update

--- tinybs/core/TinyBS/SimpleMvc/Controller/TinyBsBaseController.php
+++ tinybs/core/TinyBS/SimpleMvc/Controller/TinyBsBaseController.php
@@ -3,6 +3,7 @@ namespace TinyBS\SimpleMvc\Controller;
 
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\View\Helper\Url;
 
 abstract class TinyBsBaseController implements ServiceLocatorAwareInterface
 {
@@ -13,12 +14,21 @@ abstract class TinyBsBaseController implements ServiceLocatorAwareInterface
     public function getServiceLocator(){
         return $this->serviceLocator;
     }
-    
-    protected function getViewHeplerUrl(){
+
+    /**
+     * @return \Zend\View\Helper\Url
+     */
+    protected function getViewHelperUrl(){
     	if(!$this->viewHelperUrl)
     		$this->viewHelperUrl = $this->getServiceLocator()->get('TinyBS\View\Helper\Url');
     	return $this->viewHelperUrl;
     }
+
+    protected function redirect($url){
+        header("Location: "+$url);
+        exit;
+    }
+
     private $serviceLocator = null;
     private $viewHelperUrl = null;
 }
