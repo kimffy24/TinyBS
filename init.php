8f09b8418608ff873d4e19a80f18898f5f561eef
kimffy24 <kimffy24@gmail.com>
2015-05-27 15:33:03 +0000

    set the main container between ob_start() and ob_end_flush()

--- init.php
+++ init.php
@@ -4,6 +4,7 @@
  * check library ZendFramework2 is loaded
  * load TinyBS core file
  */
+ob_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('TINYBSROOT', __DIR__);
 
@@ -15,8 +16,10 @@ if (!class_exists('Zend\ServiceManager\ServiceManager'))
 	throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install`.');
 
 //set error or exception handler to deal it with tbs inner handler!
+/*
 set_error_handler(array('\TinyBS\BootStrap\ExtendHandler', 'errorHandler'));
-set_exception_handler(array('\TinyBS\BootStrap\ExtendHandler', 'exceptionHandler'));
+set_exception_handler(array('\TinyBS\BootStrap\ExtendHandler', 'exceptionHandler'));*/
 
 
 TinyBS\BootStrap\BootStrap::run();
+ob_end_flush();
