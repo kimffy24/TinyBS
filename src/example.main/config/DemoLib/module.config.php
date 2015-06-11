146a8b49a035b7fe2bf0af5e34b96c2a86530582
kimffy24 <kimffy24@gmail.com>
2015-06-12 01:04:58 +0800

    test

--- src/example.main/config/DemoLib/module.config.php
+++ src/example.main/config/DemoLib/module.config.php
@@ -1,9 +1,11 @@
 <?php
+use DemoLib\Utils\FirstLib;
+
 return array(
     'service_manager' => array(
         'factories' => array(
             'FirstLib' => function ($sm){
-                return new DemoLib\Utils\FirstLib();
+                return new FirstLib();
             }
         )
     )
